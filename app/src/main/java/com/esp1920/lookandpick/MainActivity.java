8bc145f7dc6cdb842e2d65ef51fafbf65327272c
Alessandra Tonin <ale.tonin.98@gmail.com>
2020-05-16 09:46:12 +0200

    Added permissions and some assets from the Google VR SDK's sample

--- app/src/main/java/com/esp1920/lookandpick/MainActivity.java
+++ app/src/main/java/com/esp1920/lookandpick/MainActivity.java
@@ -4,7 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+/**
+ * This is the main activity of LookAndPick: the user is in a room and different objects will
+ * appear randomly. When in Cardboard mode, the user has to pick them up by looking them and
+ * triggering the Cardboard button.
+ */
 public class MainActivity extends AppCompatActivity {
+
+    //private static final String OBJECT_SOUND_FILE = "audio/HelloVR_Loop.ogg";
+    //private static final String SUCCESS_SOUND_FILE = "audio/HelloVR_Activation.ogg";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
