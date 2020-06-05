6b8b47aa17d241046fb69b36ccd19ca90af31bfe
laurabragagnolo <62933193+laurabragagnolo@users.noreply.github.com>
2020-06-06 00:19:52 +0200

    Create Movement.java

---
+++ app/src/main/java/com/esp1920/lookandpick/Movement.java
@@ -0,0 +1,36 @@
+package com.esp1920.lookandpick;
+
+/**
+ * Class to handle user movement in the scene
+ */
+public class Movement {
+
+    //angle between 0 and 90 degrees, if the user tilts the cardboard viewer down with
+    //an angle greater than this threshold angle, the player will start moving
+    //in the direction the head is looking at
+    private final float THRESHOLD_ANGLE = 25;
+
+    private boolean isWalking = false;
+
+    //TODO: speed private?
+    //public field so changes are more convenient (in my opinion)
+    public float speed;
+
+    private Movement() {}
+
+    /**
+     * Checks if the "walking conditions" are satisfied
+     * TODO: write a better comment
+     */
+    public void isWalking(){
+
+    }
+
+    /**
+     * Makes user walk forward -> makes the scene move backwards
+     * TODO: write a better comment
+     */
+    public void walk(){
+
+    }
+}
