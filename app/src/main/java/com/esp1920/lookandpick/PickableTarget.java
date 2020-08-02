bc3a8a7549b53970dd4612fd25b5dc80f6d9aa55
Alessandra Tonin <ale.tonin.98@gmail.com>
2020-08-03 00:12:46 +0200

    Fixed a small error on timer creation

--- app/src/main/java/com/esp1920/lookandpick/PickableTarget.java
+++ app/src/main/java/com/esp1920/lookandpick/PickableTarget.java
@@ -28,6 +28,8 @@ public class PickableTarget {
         mPosition = new Position();
         mPosition.generateRandomPosition();
         setMeshIndex(index);
+        mTimer = new DisappearanceTimer();
+
     }
 
     /**
