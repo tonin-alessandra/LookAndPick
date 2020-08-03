c4b9ff61584699e731860f9a1c980bb10da23fe3
laurabragagnolo <62933193+laurabragagnolo@users.noreply.github.com>
2020-08-03 09:43:30 +0200

    Update PlayerMovement.java

--- app/src/main/java/com/esp1920/lookandpick/PlayerMovement.java
+++ app/src/main/java/com/esp1920/lookandpick/PlayerMovement.java
@@ -1,9 +1,7 @@
 package com.esp1920.lookandpick;
 
-import android.opengl.Matrix;
-import android.widget.Toast;
-
 import com.google.vr.sdk.base.HeadTransform;
+
 
 /**
  * Class to handle user movement in the scene
@@ -15,23 +13,33 @@ import com.google.vr.sdk.base.HeadTransform;
  */
  public class PlayerMovement {
 
-    private final double THRESHOLD_ANGLE = 35; // degrees
+    private final double THRESHOLD_ANGLE = 30; // degrees
+
+    private final int FORWARD = 1;
+    private final int BACKWARD = -1;
+    private final int STILL = 0;
 
     private float[] eulerAngles;
     private float[] forwardVec;
 
     private double pitch, yaw, roll;
+    private double prevYaw;
 
-    private double oldYaw;
+    private int direction;
+    private int prevDirection;
 
-    //walking speed
+    private float eyeZ;
+
     private float step;
+    private float speed;
 
     public PlayerMovement() {
         eulerAngles = new float[4];
         forwardVec = new float[3];
-        step = 0.3f;
-        oldYaw = 0.0f;
+        step = 0.25f;
+        speed = 0.009f;
+        prevYaw = 0.0f;
+        prevDirection = -2;
     }
 
     /**
@@ -43,14 +51,13 @@ import com.google.vr.sdk.base.HeadTransform;
         headTransform.getEulerAngles(eulerAngles, 0);
         pitch = Math.toDegrees(eulerAngles[0]); // Y
         yaw = Math.toDegrees(eulerAngles[1]); // Z
-        roll = Math.toDegrees(eulerAngles[2]); // X
+        //roll = Math.toDegrees(eulerAngles[2]); // X
 
-        //TODO: non int ma define
-        if(pitch <= THRESHOLD_ANGLE*-1){ return 1; }
+        if (pitch <= THRESHOLD_ANGLE * -1) { return FORWARD; }
 
-        if(pitch >= THRESHOLD_ANGLE){ return 2; }
+        if(pitch >= THRESHOLD_ANGLE){ return BACKWARD; }
 
-        return 0;
+        return STILL;
     }
 
     /**
@@ -58,36 +65,48 @@ import com.google.vr.sdk.base.HeadTransform;
      *
      */
     public String updateEyePosition (HeadTransform headTransform, float[] eyePosition) {
-        int a = isWalking(headTransform);
-        if(a == 0) return "";
+        direction = isWalking(headTransform);
+        if(direction == STILL) return "";
 
         headTransform.getForwardVector(forwardVec, 0);
-        headTransform.getEulerAngles(eulerAngles, 0);
-
-        yaw = eulerAngles[1];
-        if (Math.abs(yaw - oldYaw) >= 30 ){ step = 0.3f; }
-
-        if(a == 2){
-            eyePosition[2] = forwardVec[2] * -1 * nextStep();
+        eyeZ = eyePosition[2];
+        if(direction == FORWARD && canGoForward()){
+                eyePosition[2] = (eyeZ + nextStep()) * forwardVec[2];
         }
-        if(a == 1){
-            if (eyePosition[2] < -0.7 && Math.abs(yaw) < 20){ return "stop"; }
-            eyePosition[2] = forwardVec[2] * 0.8f * nextStep();
+        if(direction == BACKWARD && canGoBackwards()){
+                eyePosition[2] = (eyeZ + nextStep()) * forwardVec[2] * -1;
         }
-        oldYaw = yaw;
-        return  "dir: "+String.valueOf(a)+
-                "x: "+String.valueOf(forwardVec[0])+"y: "+String.valueOf(forwardVec[1])
-                +"z: "+String.valueOf(forwardVec[2])+"eye: "+String.valueOf(eyePosition[2]);
-
-
-
+        return  //"dir: "+String.valueOf(direction)+
+                // "pitch: "+String.valueOf(eulerAngles[0])+"yaw: "+String.valueOf(eulerAngles[1])
+                "eyeX : "+String.valueOf(eyePosition[0])+" eyeY: "+String.valueOf(eyePosition[1])+
+                " eyeZ: "+String.valueOf(eyePosition[2]);
     }
 
     private float nextStep(){
-        return step += 0.05f;
+        /*if (Math.abs(yaw - prevYaw) >= 2 ){
+            prevYaw = yaw;
+            return step = 0.25f;
+        }*/
+
+        if(direction != prevDirection){
+            prevDirection = direction;
+            return step = (eyeZ * 2 - 0.25f) * -1;
+        }
+
+        return step += speed;
     }
 
+    private boolean canGoForward(){
+        if (eyeZ < -0.9f) {
+            return false;
+        }
+        return true;
+    }
 
-
-
+    private boolean canGoBackwards(){
+        if (eyeZ > 6.5f) {
+            return false;
+        }
+        return true;
+    }
 }
