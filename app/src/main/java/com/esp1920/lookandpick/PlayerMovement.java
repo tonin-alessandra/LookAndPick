b97b29a44e7a317d9361813886bbfec2a2186ca0
laurabragagnolo <62933193+laurabragagnolo@users.noreply.github.com>
2020-08-05 10:39:33 +0200

    Update PlayerMovement.java

--- app/src/main/java/com/esp1920/lookandpick/PlayerMovement.java
+++ app/src/main/java/com/esp1920/lookandpick/PlayerMovement.java
@@ -4,23 +4,30 @@ import com.google.vr.sdk.base.HeadTransform;
 
 
 /**
- * Class to handle user movement in the scene
+ * Class to handle user movement along Z axis.
  *
- * If the user tilts the cardboard viewer down with a certain angle, the player will take a step
- * forward in the direction the head is looking at.
- * If the user tilts the cardboard viewer up with a certain angle, the player will move backwards.
+ * If the user tilts the cardboard viewer down with an angle greater than the threshold, the player
+ * will start moving forward in the direction the head is looking at.
+ * If the user tilts the cardboard viewer up with an angle greater than the threshold, the player
+ * will move backwards.
+ * Movement is enabled only if the user is looking at the wall with purple tiles or the wall with
+ * blue tiles, these are, respectively, the wall ahead and the wall behind when the game begins.
+ *
+ * This feature is meant to be tested using a Cardboard viewer.
  *
  */
  public class PlayerMovement {
 
     private static final double THRESHOLD_ANGLE = 30; // degrees
 
+    // direction represents the type of movement, forward or backward, depending on the viewer tilt
     private static final int FORWARD = 1;
     private static final int BACKWARD = -1;
     private static final int STILL = 0;
     private int prevDirection = -2;
     private int direction;
 
+    //orientation represent the wall the user is facing
     private static final int AHEAD = -1;
     private static final int BEHIND = 1;
     private int prevOrientation = 0;
@@ -29,17 +36,21 @@ import com.google.vr.sdk.base.HeadTransform;
     private float[] eulerAngles = new float[4];
     private float[] forwardVec = new float[3];
 
+    // movement around Y axis
     private double pitch;
 
     private float newEyeZ;
 
-    private float step = 0.25f;
-    private float speed = 0.009f;
-    //speed moving towards the wall ahead (purple tiles)
-    //private float speedB = 0.006f; // speed moving towards the wall behind (blue tiles)
+    private static final float START = 0.25f;
+    private static final float SPEED = 0.03f;
+    private float step;
+
+    private float compensation = 0.65f;
 
     private boolean canMoveForward;
     private boolean canMoveBackward;
+    private static  final float boundA = -0.9f;
+    private static final float boundB = 5.5f;
 
     public PlayerMovement() {}
 
@@ -51,29 +62,43 @@ import com.google.vr.sdk.base.HeadTransform;
     private int isWalking(float[] eulerAngles){
         pitch = Math.toDegrees(eulerAngles[0]); // Y
 
-        if (pitch <= THRESHOLD_ANGLE * -1) { return FORWARD; }
+        if(pitch <= THRESHOLD_ANGLE * -1){
+            return FORWARD;
+        }
 
-        if(pitch >= THRESHOLD_ANGLE){ return BACKWARD; }
+        if(pitch >= THRESHOLD_ANGLE){
+            return BACKWARD;
+        }
 
         return STILL;
     }
 
     /**
-     * Calculates eyes's coordinates in order to achieve movement
-     *
+     * Calculates eyes's coordinates in order to achieve movement, according to direction and
+     * orientation.
      */
     public float updateEyePosition (HeadTransform headTransform, float eyeZ) {
-        headTransform.getForwardVector(forwardVec, 0);
-        headTransform.getEulerAngles(eulerAngles, 0);
+        headTransform.getForwardVector(forwardVec, 0); // (X, Y, Z)
+        headTransform.getEulerAngles(eulerAngles, 0);  // (pitch, yaw, roll)
+
         orientation = (int)Math.signum(forwardVec[2]);
         direction = isWalking(eulerAngles);
+
         newEyeZ = eyeZ;
+
         checkBounds();
+
         if(direction == FORWARD && canMoveForward){
-                newEyeZ = (eyeZ + nextStep()) * forwardVec[2];
+            newEyeZ = (eyeZ + nextStep()) * forwardVec[2];
+            if(orientation == BEHIND){
+                newEyeZ *= compensation;
+            }
         }
         else if(direction == BACKWARD && canMoveBackward){
-                newEyeZ = (eyeZ + nextStep()) * forwardVec[2] * -1;
+            newEyeZ = (eyeZ + nextStep()) * forwardVec[2] * -1;
+            if(orientation == AHEAD){
+                newEyeZ *= compensation;
+            }
         }
         return newEyeZ;
     }
@@ -83,35 +108,50 @@ import com.google.vr.sdk.base.HeadTransform;
             prevDirection = direction;
             if (orientation == AHEAD){
                 if(direction == FORWARD ){
-                    return step = (newEyeZ * 2 - 0.25f) * -1;
+                    return step = (newEyeZ * 2 - START) * -1;
                 }
                 if(direction == BACKWARD){
-                    return step = 0.25f;
+                    return step = START;
                 }
             }
             if (orientation == BEHIND){
                 if(direction == FORWARD ){
-                    return step = 0.25f;
+                    return step = START;
                 }
                 if(direction == BACKWARD){
-                    return step = (newEyeZ * 2 - 0.25f) * -1;
+                    return step = (newEyeZ * 2 - START) * -1;
                 }
             }
         }
         if(orientation != prevOrientation){
             prevOrientation = orientation;
-            return step = 0.25f; //TODO: non va bene praticamente mai
+            if(direction == FORWARD){
+                if(orientation == BEHIND){
+                    return step = START;
+                }
+                if(orientation == AHEAD){
+                    return step = (newEyeZ * 2 - START) * -1;
+                }
+            }
+            if(direction == BACKWARD){
+                if(orientation == BEHIND){
+                    return step = (newEyeZ * 2 - START) * -1;
+                }
+                if(orientation == AHEAD){
+                    return step = START;
+                }
+            }
         }
-        return step += speed;
+        return step += SPEED;
     }
 
     private void checkBounds(){
-        if ((orientation == AHEAD && newEyeZ < -0.9f) || (orientation == BEHIND && newEyeZ > 5.5f)) {
+        if ((orientation == AHEAD && newEyeZ < boundA) || (orientation == BEHIND && newEyeZ > boundB)) {
             canMoveForward = false;
         } else{
             canMoveForward = true;
         }
-        if ((orientation == AHEAD && newEyeZ > 5.5f) || (orientation == BEHIND && newEyeZ < -0.9f)) {
+        if ((orientation == AHEAD && newEyeZ > boundB) || (orientation == BEHIND && newEyeZ < boundA)) {
             canMoveBackward = false;
         } else{
             canMoveBackward = true;
